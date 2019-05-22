3c6e23d68af794ad659f745764ef70ad538b76c7
ISoon14 <48014332+ISoon14@users.noreply.github.com>
2019-05-22 16:52:31 +0200

    Demarrage pc

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 module.exports = function(sails) {
 
 
-	function wakeLan(mac){
-		
+	function wakeLan(){
 		return gladys.param.getValues(['WAKEONLAN_mac'])
         .spread((WAKEONLAN_mac) => {
-            console.log(WAKEONLAN_mac);
+	            var wol = require('node-wol');
+	 
+		wol.wake(WAKEONLAN_mac);
+		 
+		wol.wake(WAKEONLAN_mac, function(error) {
+		  if(error) {
+		    return reject(new Error('Erreur lors du démarrage'));
+		  }
+		});
+		 
+		var magicPacket = wol.createMagicPacket(WAKEONLAN_mac);
         });
 	}
 
