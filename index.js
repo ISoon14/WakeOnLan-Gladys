d22acfeb2461ed39b59b26a8bce2354340c6b06b
ISoon14 <48014332+ISoon14@users.noreply.github.com>
2019-05-22 18:13:41 +0200

    Install

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 module.exports = function(sails) {
 
-
+	install: require('./lib/install'),
+	
 	function wakeLan(){
 		return gladys.param.getValues(['WAKEONLAN_mac'])
         .spread((WAKEONLAN_mac) => {
 	    
 		    const wol = require('wol');
-		    
+
 			wol.wake(WAKEONLAN_mac, function(err, res){
 			  console.log(res);
 			});
