2c15da436d5afc26d8ce75d4599d23ca4d04b04c
ISoon14 <48014332+ISoon14@users.noreply.github.com>
2019-05-22 16:55:45 +0200

    t

--- index.js
+++ index.js
@@ -4,17 +4,17 @@ module.exports = function(sails) {
 	function wakeLan(){
 		return gladys.param.getValues(['WAKEONLAN_mac'])
         .spread((WAKEONLAN_mac) => {
-	            var wol = require('node-wol');
-	 
+	    
+	    var wol = require('node-wol');
 		wol.wake(WAKEONLAN_mac);
-		 
 		wol.wake(WAKEONLAN_mac, function(error) {
 		  if(error) {
 		    return reject(new Error('Erreur lors du démarrage'));
 		  }
 		});
 		 
-		var magicPacket = wol.createMagicPacket(WAKEONLAN_mac);
+		//var magicPacket = wol.createMagicPacket(WAKEONLAN_mac);
+
         });
 	}
 
