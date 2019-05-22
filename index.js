978577ca265fdaa6254b0287380ff596d1c865e4
ISoon14 <48014332+ISoon14@users.noreply.github.com>
2019-05-22 19:29:56 +0200

    tes

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ module.exports = function(sails) {
 
 
 	return {
-		install: require('./lib/install.js'),
+		install: require('./lib/install.'),
 		wakeLan:require('./lib/wakelan.js'),
 		command:require('./lib/command.js')
 	};
